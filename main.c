9a99ab7e21136b6e28c3f912edb9db09cdd8613e
Samuel Oxandale <soxand16@gmail.com>
2024-03-15 13:14:25 -0600

    added projects

---
+++ main.c
@@ -0,0 +1,223 @@
+/*******************************************************************************
+ *
+ * ECE 431: Lab 7
+ *
+ ******************************************************************************/
+#include <driverlib.h>
+
+#define OUTOFBOX_MSP430FR6989_HAL_LCD_H_
+
+//Change based on LCD Memory locations
+#define pos1 9   /* Digit A1 begins at S18 */
+#define pos2 5   /* Digit A2 begins at S10 */
+#define pos3 3   /* Digit A3 begins at S6  */
+#define pos4 18  /* Digit A4 begins at S36 */
+#define pos5 14  /* Digit A5 begins at S28 */
+#define pos6 7   /* Digit A6 begins at S14 */
+
+// Define word access definitions to LCD memories
+#define LCDMEMW    ((int*) LCDMEM) /* LCD Memory (for C) */
+
+// Initialization calls
+void show_digit(int, int);
+void set_time(int, int, int);
+void display_time(void);
+void init_GPIO(void);
+void init_LCD(void);
+
+// LCD memory map for numeric digits
+const char digit[10][2] =
+{
+    {0xFC, 0x28},  /* "0" LCD segments a+b+c+d+e+f+k+q */
+    {0x60, 0x20},  /* "1" */
+    {0xDB, 0x00},  /* "2" */
+    {0xF3, 0x00},  /* "3" */
+    {0x67, 0x00},  /* "4" */
+    {0xB7, 0x00},  /* "5" */
+    {0xBF, 0x00},  /* "6" */
+    {0xE4, 0x00},  /* "7" */
+    {0xFF, 0x00},  /* "8" */
+    {0xF7, 0x00}   /* "9" */
+};
+Calendar currentTime;
+
+/*
+ * Main routine
+ */
+int main(void)
+{
+    // Stop watchdog timer
+    WDT_A_hold(__MSP430_BASEADDRESS_WDT_A__);
+
+    // Initialize pins to output
+    init_GPIO();
+
+    // Initialize LCD
+    init_LCD();
+
+    // Set initial time
+    set_time(23, 59, 50);
+
+    // Set RTC counter to trigger interrupt every ~250 ms
+    RTC_C_initCounter(RTC_C_BASE, RTC_C_CLOCKSELECT_32KHZ_OSC, RTC_C_COUNTERSIZE_16BIT);
+    RTC_C_startClock(RTC_C_BASE);
+
+    while(1)
+    {
+        display_time();
+    }
+}
+
+/*
+ * Displays input digit at given LCD digit/position
+ */
+void show_digit(int d, int position)
+{
+    // Display digit
+    LCDMEM[position] = digit[d][0];
+    LCDMEM[position+1] = digit[d][1];
+}
+
+/*
+ * Sets the time and displays it
+ */
+void set_time(int hours, int minutes, int seconds)
+{
+    // Hold clock
+    RTC_C_holdClock(RTC_C_BASE);
+
+    currentTime.Hours = hours;
+    currentTime.Minutes = minutes;
+    currentTime.Seconds = seconds;
+    currentTime.DayOfWeek = 0x04;
+    currentTime.DayOfMonth = 0x30;
+    currentTime.Month = 0x04;
+    currentTime.Year = 0x2015;
+
+
+    RTC_C_initCalendar(RTC_C_BASE,
+                       &currentTime,
+                       RTC_C_FORMAT_BINARY);
+
+    // Update LCD with new time
+    display_time();
+}
+
+/*
+ * Displays the currentTime on the LCD
+ */
+void display_time()
+{
+    currentTime = RTC_C_getCalendarTime(RTC_C_BASE);
+
+    show_digit((currentTime.Seconds) % 10, pos6);
+    show_digit((currentTime.Seconds) / 10, pos5);
+    show_digit((currentTime.Minutes) % 10, pos4);
+    show_digit((currentTime.Minutes) / 10, pos3);
+    show_digit((currentTime.Hours) % 10, pos2);
+    show_digit((currentTime.Hours) / 10, pos1);
+
+    // Display the colons
+    LCDM7 |= 0x04;
+    LCDM20 |= 0x04;
+}
+
+/*
+ * GPIO Initialization
+ */
+void init_GPIO()
+{
+    // Set all GPIO pins to output low to prevent floating input and reduce power consumption
+    GPIO_setOutputLowOnPin(GPIO_PORT_P1, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setOutputLowOnPin(GPIO_PORT_P2, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setOutputLowOnPin(GPIO_PORT_P3, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setOutputLowOnPin(GPIO_PORT_P4, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setOutputLowOnPin(GPIO_PORT_P5, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setOutputLowOnPin(GPIO_PORT_P6, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setOutputLowOnPin(GPIO_PORT_P7, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setOutputLowOnPin(GPIO_PORT_P8, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setOutputLowOnPin(GPIO_PORT_P9, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+
+    GPIO_setAsOutputPin(GPIO_PORT_P1, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setAsOutputPin(GPIO_PORT_P2, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setAsOutputPin(GPIO_PORT_P3, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setAsOutputPin(GPIO_PORT_P4, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setAsOutputPin(GPIO_PORT_P5, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setAsOutputPin(GPIO_PORT_P6, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setAsOutputPin(GPIO_PORT_P7, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setAsOutputPin(GPIO_PORT_P8, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setAsOutputPin(GPIO_PORT_P9, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+
+    GPIO_setAsInputPin(GPIO_PORT_P3, GPIO_PIN5);
+
+    // Configure button S1 (P1.1) interrupt
+    GPIO_selectInterruptEdge(GPIO_PORT_P1, GPIO_PIN1, GPIO_HIGH_TO_LOW_TRANSITION);
+    GPIO_setAsInputPinWithPullUpResistor(GPIO_PORT_P1, GPIO_PIN1);
+    GPIO_clearInterrupt(GPIO_PORT_P1, GPIO_PIN1);
+    GPIO_enableInterrupt(GPIO_PORT_P1, GPIO_PIN1);
+
+    // Configure button S2 (P1.2) interrupt
+    GPIO_selectInterruptEdge(GPIO_PORT_P1, GPIO_PIN2, GPIO_HIGH_TO_LOW_TRANSITION);
+    GPIO_setAsInputPinWithPullUpResistor(GPIO_PORT_P1, GPIO_PIN2);
+    GPIO_clearInterrupt(GPIO_PORT_P1, GPIO_PIN2);
+    GPIO_enableInterrupt(GPIO_PORT_P1, GPIO_PIN2);
+
+    // Set P4.1 and P4.2 as Secondary Module Function Input, LFXT.
+    GPIO_setAsPeripheralModuleFunctionInputPin(
+           GPIO_PORT_PJ,
+           GPIO_PIN4 + GPIO_PIN5,
+           GPIO_PRIMARY_MODULE_FUNCTION
+           );
+
+    // Disable the GPIO power-on default high-impedance mode
+    // to activate previously configured port settings
+    PMM_unlockLPM5();
+}
+
+void init_LCD()
+{
+    LCD_C_initParam initParams = {0};
+    initParams.clockSource = LCD_C_CLOCKSOURCE_ACLK;
+    initParams.clockDivider = LCD_C_CLOCKDIVIDER_1;
+    initParams.clockPrescalar = LCD_C_CLOCKPRESCALAR_16;
+    initParams.muxRate = LCD_C_4_MUX;
+    initParams.waveforms = LCD_C_LOW_POWER_WAVEFORMS;
+    initParams.segments = LCD_C_SEGMENTS_ENABLED;
+
+    LCD_C_init(LCD_C_BASE, &initParams);
+    // LCD Operation - VLCD generated internally, V2-V4 generated internally, v5 to ground
+
+    /*  'FR6989 LaunchPad LCD1 uses Segments S4, S6-S21, S27-S31 and S35-S39 */
+    LCD_C_setPinAsLCDFunctionEx(LCD_C_BASE, LCD_C_SEGMENT_LINE_4,
+                                LCD_C_SEGMENT_LINE_4);
+    LCD_C_setPinAsLCDFunctionEx(LCD_C_BASE, LCD_C_SEGMENT_LINE_6,
+                                LCD_C_SEGMENT_LINE_21);
+    LCD_C_setPinAsLCDFunctionEx(LCD_C_BASE, LCD_C_SEGMENT_LINE_27,
+                                LCD_C_SEGMENT_LINE_31);
+    LCD_C_setPinAsLCDFunctionEx(LCD_C_BASE, LCD_C_SEGMENT_LINE_35,
+                                LCD_C_SEGMENT_LINE_39);
+
+    LCD_C_setVLCDSource(LCD_C_BASE, LCD_C_VLCD_GENERATED_INTERNALLY,
+                        LCD_C_V2V3V4_GENERATED_INTERNALLY_NOT_SWITCHED_TO_PINS,
+                        LCD_C_V5_VSS);
+
+    // Set VLCD voltage to 3.20v
+    LCD_C_setVLCDVoltage(LCD_C_BASE,
+                         LCD_C_CHARGEPUMP_VOLTAGE_3_02V_OR_2_52VREF);
+
+    // Enable charge pump and select internal reference for it
+    LCD_C_enableChargePump(LCD_C_BASE);
+    LCD_C_selectChargePumpReference(LCD_C_BASE,
+                                    LCD_C_INTERNAL_REFERENCE_VOLTAGE);
+
+    LCD_C_configChargePump(LCD_C_BASE, LCD_C_SYNCHRONIZATION_ENABLED, 0);
+
+    // Clear LCD memory
+    LCD_C_clearMemory(LCD_C_BASE);
+
+    //Turn LCD on
+    LCD_C_on(LCD_C_BASE);
+
+    LCD_C_selectDisplayMemory(LCD_C_BASE, LCD_C_DISPLAYSOURCE_MEMORY);
+}
+
---
+++ main_shell.c
@@ -0,0 +1,210 @@
+/*******************************************************************************
+ *
+ * ECE 431: Lab 7 Shell
+ *
+ ******************************************************************************/
+#include <driverlib.h>
+
+#define OUTOFBOX_MSP430FR6989_HAL_LCD_H_
+
+//Change based on LCD Memory locations
+#define pos1 9   /* Digit A1 begins at S18 */
+#define pos2 5   /* Digit A2 begins at S10 */
+#define pos3 3   /* Digit A3 begins at S6  */
+#define pos4 18  /* Digit A4 begins at S36 */
+#define pos5 14  /* Digit A5 begins at S28 */
+#define pos6 7   /* Digit A6 begins at S14 */
+
+// Define word access definitions to LCD memories
+#define LCDMEMW    ((int*) LCDMEM) /* LCD Memory (for C) */
+
+// Initialization calls
+void show_digit(int, int);
+void set_time(int, int, int);
+void display_time(void);
+void inc_time(void);
+void init_GPIO(void);
+void init_LCD(void);
+
+// LCD memory map for numeric digits
+const char digit[10][2] =
+{
+    {0xFC, 0x28},  /* "0" LCD segments a+b+c+d+e+f+k+q */
+    {0x60, 0x20},  /* "1" */
+    {0xDB, 0x00},  /* "2" */
+    {0xF3, 0x00},  /* "3" */
+    {0x67, 0x00},  /* "4" */
+    {0xB7, 0x00},  /* "5" */
+    {0xBF, 0x00},  /* "6" */
+    {0xE4, 0x00},  /* "7" */
+    {0xFF, 0x00},  /* "8" */
+    {0xF7, 0x00}   /* "9" */
+};
+Calendar currentTime;
+
+/*
+ * Main routine
+ * TODO: Complete clock functionality
+ */
+int main(void)
+{
+    // Stop watchdog timer
+    WDT_A_hold(__MSP430_BASEADDRESS_WDT_A__);
+    // Initialize pins to output
+    init_GPIO();
+    // Initialize LCD
+    init_LCD();
+
+    // TODO: Setup counter, clock, and initial set time
+
+
+    while(1)
+    {
+        // TODO: Loop that updates displayed time.
+    }
+}
+
+/*
+ * Displays input digit at given LCD digit/position
+ *
+ * TODO: Implement function. The position of the
+ *       LCD characters are defined using the global
+ *       variables pos1, pos2, ..., pos6. Note: Each
+ *       of these has more than 7 segments; each digit
+ *       pulls from two memory locations. The values
+ *       corresponding with digits is given in the
+ *       2D digit array.
+ *
+ *       Hint: Shortcut for LCD memory is LCDMEM
+ */
+void show_digit(int d, int position)
+{
+
+}
+
+/*
+ * Retrieves the currentTime and displays it
+ *
+ * TODO: Implement function. The time is stored in the
+ *       global Calendar struct currentTime. Go to the
+ *       declaration of Calendar for more info on it.
+ *       You must also update the counter.
+ */
+void set_time(int hours, int minutes, int seconds)
+{
+
+}
+
+/*
+ * Retrieves the currentTime from the counter and
+ * displays it on the LCD
+ *
+ * TODO: Implement function. This should be relatively
+ *       straightforward after implementing the show
+ *       digit function and initializing the counter.
+ */
+void display_time()
+{
+
+}
+
+
+/*
+ * GPIO Initialization
+ */
+void init_GPIO()
+{
+    // Set all GPIO pins to output low to prevent floating input and reduce power consumption
+    GPIO_setOutputLowOnPin(GPIO_PORT_P1, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setOutputLowOnPin(GPIO_PORT_P2, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setOutputLowOnPin(GPIO_PORT_P3, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setOutputLowOnPin(GPIO_PORT_P4, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setOutputLowOnPin(GPIO_PORT_P5, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setOutputLowOnPin(GPIO_PORT_P6, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setOutputLowOnPin(GPIO_PORT_P7, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setOutputLowOnPin(GPIO_PORT_P8, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setOutputLowOnPin(GPIO_PORT_P9, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+
+    GPIO_setAsOutputPin(GPIO_PORT_P1, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setAsOutputPin(GPIO_PORT_P2, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setAsOutputPin(GPIO_PORT_P3, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setAsOutputPin(GPIO_PORT_P4, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setAsOutputPin(GPIO_PORT_P5, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setAsOutputPin(GPIO_PORT_P6, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setAsOutputPin(GPIO_PORT_P7, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setAsOutputPin(GPIO_PORT_P8, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setAsOutputPin(GPIO_PORT_P9, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+
+    GPIO_setAsInputPin(GPIO_PORT_P3, GPIO_PIN5);
+
+    // Configure button S1 (P1.1) interrupt
+    GPIO_selectInterruptEdge(GPIO_PORT_P1, GPIO_PIN1, GPIO_HIGH_TO_LOW_TRANSITION);
+    GPIO_setAsInputPinWithPullUpResistor(GPIO_PORT_P1, GPIO_PIN1);
+    GPIO_clearInterrupt(GPIO_PORT_P1, GPIO_PIN1);
+    GPIO_enableInterrupt(GPIO_PORT_P1, GPIO_PIN1);
+
+    // Configure button S2 (P1.2) interrupt
+    GPIO_selectInterruptEdge(GPIO_PORT_P1, GPIO_PIN2, GPIO_HIGH_TO_LOW_TRANSITION);
+    GPIO_setAsInputPinWithPullUpResistor(GPIO_PORT_P1, GPIO_PIN2);
+    GPIO_clearInterrupt(GPIO_PORT_P1, GPIO_PIN2);
+    GPIO_enableInterrupt(GPIO_PORT_P1, GPIO_PIN2);
+
+    // Set P4.1 and P4.2 as Secondary Module Function Input, LFXT.
+    GPIO_setAsPeripheralModuleFunctionInputPin(
+           GPIO_PORT_PJ,
+           GPIO_PIN4 + GPIO_PIN5,
+           GPIO_PRIMARY_MODULE_FUNCTION
+           );
+
+    // Disable the GPIO power-on default high-impedance mode
+    // to activate previously configured port settings
+    PMM_unlockLPM5();
+}
+
+void init_LCD()
+{
+    LCD_C_initParam initParams = {0};
+    initParams.clockSource = LCD_C_CLOCKSOURCE_ACLK;
+    initParams.clockDivider = LCD_C_CLOCKDIVIDER_1;
+    initParams.clockPrescalar = LCD_C_CLOCKPRESCALAR_16;
+    initParams.muxRate = LCD_C_4_MUX;
+    initParams.waveforms = LCD_C_LOW_POWER_WAVEFORMS;
+    initParams.segments = LCD_C_SEGMENTS_ENABLED;
+
+    LCD_C_init(LCD_C_BASE, &initParams);
+    // LCD Operation - VLCD generated internally, V2-V4 generated internally, v5 to ground
+
+    /*  'FR6989 LaunchPad LCD1 uses Segments S4, S6-S21, S27-S31 and S35-S39 */
+    LCD_C_setPinAsLCDFunctionEx(LCD_C_BASE, LCD_C_SEGMENT_LINE_4,
+                                LCD_C_SEGMENT_LINE_4);
+    LCD_C_setPinAsLCDFunctionEx(LCD_C_BASE, LCD_C_SEGMENT_LINE_6,
+                                LCD_C_SEGMENT_LINE_21);
+    LCD_C_setPinAsLCDFunctionEx(LCD_C_BASE, LCD_C_SEGMENT_LINE_27,
+                                LCD_C_SEGMENT_LINE_31);
+    LCD_C_setPinAsLCDFunctionEx(LCD_C_BASE, LCD_C_SEGMENT_LINE_35,
+                                LCD_C_SEGMENT_LINE_39);
+
+    LCD_C_setVLCDSource(LCD_C_BASE, LCD_C_VLCD_GENERATED_INTERNALLY,
+                        LCD_C_V2V3V4_GENERATED_INTERNALLY_NOT_SWITCHED_TO_PINS,
+                        LCD_C_V5_VSS);
+
+    // Set VLCD voltage to 3.20v
+    LCD_C_setVLCDVoltage(LCD_C_BASE,
+                         LCD_C_CHARGEPUMP_VOLTAGE_3_02V_OR_2_52VREF);
+
+    // Enable charge pump and select internal reference for it
+    LCD_C_enableChargePump(LCD_C_BASE);
+    LCD_C_selectChargePumpReference(LCD_C_BASE,
+                                    LCD_C_INTERNAL_REFERENCE_VOLTAGE);
+
+    LCD_C_configChargePump(LCD_C_BASE, LCD_C_SYNCHRONIZATION_ENABLED, 0);
+
+    // Clear LCD memory
+    LCD_C_clearMemory(LCD_C_BASE);
+
+    //Turn LCD on
+    LCD_C_on(LCD_C_BASE);
+
+    LCD_C_selectDisplayMemory(LCD_C_BASE, LCD_C_DISPLAYSOURCE_MEMORY);
+}
+
